ff64036aabc752191180def3b0b10387a8661bf3
tezhangtiger <tezhang@microsoft.com>
2017-03-09 20:08:30 -0800

    Update dummy_gpu.R

--- dummy_gpu.R
+++ dummy_gpu.R
@@ -2,7 +2,7 @@ library(MicrosoftML)
 normData <- data.frame(matrix(rnorm(r*c,0,1),r,c))
 label = sample(c(0, 1), r, replace = T)
 normData=cbind(normData, label=as.factor(label))
-modelNN=rxNeuralNet(formula=label~., data = normData, type = "binary", acceleration = 'gpu',
+model_NN_gpu=rxNeuralNet(formula=label~., data = normData, type = "binary", acceleration = 'gpu',
                     numIterations = 100, miniBatchSize = 256, reportProgress = 0)
-modelNN=rxNeuralNet(formula=label~., data = normData, type = "binary", acceleration = 'sse',
+model_NN_sse=rxNeuralNet(formula=label~., data = normData, type = "binary", acceleration = 'sse',
                     numIterations = 100, miniBatchSize = 1, reportProgress = 0)
